374141cdb97d071b9690f1cb931d0cbfbcafd401
Pashkaatata <35433420+Pashkaatata@users.noreply.github.com>
2018-06-06 15:10:40 +0300

    Update clock.js

--- clock.js
+++ clock.js
@@ -9,7 +9,9 @@ function decOfNum(number, titles) {
 
 function timerLeft(year,month,day) {
     setInterval(function() {
-        var hoursLeft = document.getElementById('timer-v-hours'),
+        var daysLeft = document.getElementById('timer-v-days'),
+            daysText = document.getElementById('timer-v-days__text'),
+            hoursLeft = document.getElementById('timer-v-hours'),
             hoursText = document.getElementById('timer-v-hours__text'),
             minutesLeft = document.getElementById('timer-v-minutes'),
             minutesText = document.getElementById('timer-v-minutes__text'),
@@ -21,10 +23,13 @@ function timerLeft(year,month,day) {
             secToMinutes = sec % 3600,
             hoursNumber = Math.floor(sec / 3600),
             minutesNumber = Math.floor(secToMinutes / 60),
-            secondsNumber = secToMinutes % 60;
+            secondsNumber = secToMinutes % 60,
+            daysNumber = Math.floor(sec / 86400);
+        daysText.innerHTML = decOfNum(daysNumber, ['день', 'дня', 'дней']);
         hoursText.innerHTML = decOfNum(hoursNumber, ['час', 'часа', 'часов']);
         minutesText.innerHTML = decOfNum(minutesNumber, ['минута', 'минуты', 'минут']);
         secText.innerHTML = decOfNum(secondsNumber, ['секунда', 'секунды', 'секунд']);
+        daysLeft.innerHTML = daysNumber;
         hoursLeft.innerHTML = hoursNumber;
         minutesLeft.innerHTML = minutesNumber;
         secLeft.innerHTML = secondsNumber;
@@ -32,4 +37,4 @@ function timerLeft(year,month,day) {
     },1000)
 }
 
-timerLeft(2018, 6, 13);
+timerLeft(2018, 6, -17);
